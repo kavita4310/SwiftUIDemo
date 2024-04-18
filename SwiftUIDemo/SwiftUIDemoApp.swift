83157a36925132bed14d4e1cfd6706d7c987461e
kavita chauhan <kavita4310ios@gmail.com>
2024-04-18 07:12:00 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by kavita chauhan on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
